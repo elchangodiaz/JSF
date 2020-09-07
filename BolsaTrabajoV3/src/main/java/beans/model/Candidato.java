f15c645a4c687fcd88d3fe7f18eb9f387b9aaec8
javier <elchangodiaz1@gmail.com>
2020-09-06 22:33:44 -0500

    Projects upload

---
+++ BolsaTrabajoV3/src/main/java/beans/model/Candidato.java
@@ -0,0 +1,61 @@
+package beans.model;
+
+import java.util.Date;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Named
+@RequestScoped
+public class Candidato {
+
+    private String nombre;
+    private String apellido;
+    private int salarioDeseado;
+    private Date fechaNacimiento;
+
+    public Candidato() {
+        log.info("Creando el objeto Candidato");
+        this.setNombre("Introduce tu nombre");
+
+    }
+
+    Logger log = LogManager.getRootLogger();
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+        log.info("Modificando la propiedad de nombre:" + this.nombre);
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+        log.info("Modificando la propiedad de apellido " + this.apellido);
+    }
+
+    public int getSalarioDeseado() {
+        return salarioDeseado;
+    }
+
+    public void setSalarioDeseado(int salarioDeseado) {
+        this.salarioDeseado = salarioDeseado;
+        log.info("modificando la propiedad de SueldoDeseado " + this.salarioDeseado);
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+}
